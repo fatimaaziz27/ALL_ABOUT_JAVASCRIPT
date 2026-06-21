d7e40bf261e5ba818a40a0a3489f4640a7897f2f
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-06-21 22:57:37 +0500

    Update BASICS.js

--- BASICS.js
+++ BASICS.js
@@ -1,1 +1,8 @@
 
+console.log("Let's build a pyramid using JavaScript basics:");
+console.log("    #    ");
+console.log("   ###   ");
+console.log("  #####  ");
+console.log(" ####### ");
+console.log("#########");
+
