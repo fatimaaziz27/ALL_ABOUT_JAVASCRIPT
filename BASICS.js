8b09b44ebbbdb9f4690aa1630326dc737f3f7994
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-06-22 00:09:10 +0500

    Add pyramid printing and variable declaration

--- BASICS.js
+++ BASICS.js
@@ -1,3 +1,4 @@
+// PRINT FUNCTION:
 
 console.log("Let's build a pyramid using JavaScript basics:");
 console.log("    #    ");
@@ -6,3 +7,8 @@ console.log("  #####  ");
 console.log(" ####### ");
 console.log("#########");
 
+// VARIABLE:
+
+let variable;
+let word = "javascript";
+console.log(word);
