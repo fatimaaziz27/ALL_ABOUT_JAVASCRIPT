b0146c80cc4fa20fd2c6b305b40b6841179a44ea
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-06-23 01:09:01 +0500

    Update ARRAYS.js

--- ARRAYS.js
+++ ARRAYS.js
@@ -1,1 +1,10 @@
 
+let array = [];
+let array = ["first", "second"];
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+
+let obj = ["name","age","height"]
+obj [2] = "weight";
+console.log(obj);
