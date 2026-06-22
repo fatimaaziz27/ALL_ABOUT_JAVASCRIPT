854b627099a3cb89fab8ced839b2313bade58f8c
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-06-23 01:28:33 +0500

    Update ARRAYS.js

--- ARRAYS.js
+++ ARRAYS.js
@@ -13,3 +13,14 @@ console.log(obj);
 
 console.log(rows.length);
 console.log(rows.length - 1);
+
+let cities = ["london","new york","mumbai"];
+console.log(cities);
+cities[2] = "mexico"
+console.log(cities);
+cities[3] = "karachi";
+console.log(cities);
+// // output
+// [ 'london', 'new york', 'mumbai' ]
+// [ 'london', 'new york', 'mexico' ]
+// [ 'london', 'new york', 'mexico', 'karachi' ]
