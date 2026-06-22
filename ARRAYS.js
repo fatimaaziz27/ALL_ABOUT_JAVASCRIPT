e059c7c80596078f79654763b931df3c29f3543e
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-06-23 01:50:28 +0500

    Update ARRAYS.js

--- ARRAYS.js
+++ ARRAYS.js
@@ -38,3 +38,9 @@ console.log(cities);
 let rows = ["Naomi", "Quincy", "CamperChan"];
 rows.push(12);
 console.log(rows);
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+let popped = rows.pop();
+console.log(popped);
+console.log(rows);
