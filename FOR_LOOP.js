b542943e76260647dc0275e6f821de578085217d
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-06-24 00:52:50 +0500

    Update FOR_LOOP.js

--- FOR_LOOP.js
+++ FOR_LOOP.js
@@ -34,3 +34,20 @@ for (const row of rows) {
 }
 
 console.log(result);
+
+//     OR
+
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
