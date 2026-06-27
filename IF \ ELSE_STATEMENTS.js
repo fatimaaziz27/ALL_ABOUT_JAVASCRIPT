334d4ef37170aa5b523410e6f28cb9df828ff3f4
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-06-27 07:43:28 +0500

    Update IF \ ELSE_STATEMENTS.js

--- IF \ ELSE_STATEMENTS.js
+++ IF \ ELSE_STATEMENTS.js
@@ -6,3 +6,12 @@
 if (true) {
   console.log("Condition is true");
 }
+
+
+if ("") {
+  console.log("Condition is true");
+} else if (5 < 10) {
+  console.log("5 is less than 10");
+} else {
+  console.log("This is the else block");
+}
