d7cdf00bf8a226cc417a9368ee17d142c4db9405
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-06-29 02:14:09 +0500

    Update METHODS.js

--- METHODS.js
+++ METHODS.js
@@ -1,1 +1,85 @@
+// What is a Method?
+// A method is a function that belongs to an object.
 
+// Syntax
+// object.methodName();
+
+// Example
+
+const person = {
+  greet() {
+    console.log("Hello!");
+  }
+};
+
+person.greet();
+
+
+// String Methods
+
+// Syntax
+// string.methodName();
+
+// Example
+
+let text = "JavaScript";
+text.toUpperCase();
+
+// Array Methods
+
+// Syntax
+// array.methodName();
+
+// Example
+let numbers = [1, 2, 3];
+numbers.push(4);
+
+// Object Methods
+
+// Syntax
+// Object.methodName(object);
+
+// Example
+
+```javascript
+const student = {
+  name: "Ali",
+  age: 20
+};
+
+Object.keys(student);
+```
+
+---
+
+# Math Methods
+
+### Syntax
+
+```javascript
+Math.methodName();
+```
+
+### Example
+
+```javascript
+Math.sqrt(64);
+```
+
+---
+
+# Date Methods
+
+### Syntax
+
+```javascript
+date.methodName();
+```
+
+### Example
+
+```javascript
+const today = new Date();
+
+today.getFullYear();
+```
