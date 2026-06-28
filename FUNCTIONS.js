21cae84c349f8c4ce03bd46917d399546d4b1b1d
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-06-29 02:55:21 +0500

    Update FUNCTIONS.js

--- FUNCTIONS.js
+++ FUNCTIONS.js
@@ -126,3 +126,25 @@ for (const row of rows) {
 }
 
 console.log(result);
+
+
+// CODE 4 ---->
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}
+for (let i = count; i > 0; i--) {
+  rows.push(padRow(i, count));
+}
+let result = ""
+for (const row of rows) {
+  result = result + row + "\n";
+}
+console.log(result);
