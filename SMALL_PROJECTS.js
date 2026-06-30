d8eb922cd19975a4f90b3f974b0ee5ecfe765b6e
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-06-30 05:06:45 +0500

    Update SMALL_PROJECTS.js

--- SMALL_PROJECTS.js
+++ SMALL_PROJECTS.js
@@ -1,1 +1,14 @@
+// AVERAGE CALCULATOR 
+let AllMarks = [92, 88, 12, 77, 57, 100, 67, 38, 97, 89];
+
+function getAverage(scores) {
+  let sum = 0;
+  for (let i in scores){
+    sum += scores[i];
+    }
+    let average = sum/scores.length;
+    return average;
+}
+console.log(getAverage(AllMarks))
+
 
