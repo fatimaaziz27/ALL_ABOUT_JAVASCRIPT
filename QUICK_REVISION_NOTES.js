9d76e924209527ccd9cb167717ae5f468d4418d4
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-07-02 08:09:39 +0500

    Update QUICK_REVISION_NOTES.js

--- QUICK_REVISION_NOTES.js
+++ QUICK_REVISION_NOTES.js
@@ -156,3 +156,29 @@ for (let i = 1; i <= 5; i++) {
   inverted ? rows.unshift(padRow(i, 5))
            : rows.push(padRow(i, 5));
 }
+
+
+
+
+// SYNTAX ------->
+// if (condition) {
+//   logic
+// }
+
+if (true) {
+  console.log("Condition is true");
+}
+
+
+if ("") {
+  console.log("Condition is true");
+} else if (5 < 10) {
+  console.log("5 is less than 10");
+} else {
+  console.log("This is the else block");
+}
+
+
+
+
+
