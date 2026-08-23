49d7921793389696386ed0164d0e67431e1c7571
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-08-24 01:14:37 +0500

    Update CONTINUE.js

--- CONTINUE.js
+++ CONTINUE.js
@@ -1,24 +1,22 @@
-Continue Statement
-Example
+// Continue Statement
+// Example
 for (let i = 1; i < 10; i++) {
   if (i === 3) { continue; }
   text += "The number is " + i + "<br>";
 }
 
+// JavaScript Labels
+// Syntax
+// A label is an identifier followed by a colon (:).
+// labelname: statement;
+// labelname: {
+//   statements
+// }
 
-JavaScript Labels
-Syntax
-A label is an identifier followed by a colon (:).
-labelname: statement;
-
-labelname: {
-  statements
-}
-
-Continue to Labelname
-Syntax
-continue labelname;
-Example
+// Continue to Labelname
+// Syntax
+// continue labelname;
+// Example
 Continue to loop1:
 let text = "";
 loop1: for (let j = 1; j < 5; j++) {
@@ -28,7 +26,7 @@ loop1: for (let j = 1; j < 5; j++) {
    }
 }
 
-Example
+// Example
 Continue to loop2:
 let text = "";
 loop1: for (let j = 1; j < 5; j++) {
