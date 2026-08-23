653d4f2ce950180f08abad3f7d3e1cb98e3c7aa7
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-08-24 01:28:23 +0500

    Update BASIC_USEFUL_JS.js

--- BASIC_USEFUL_JS.js
+++ BASIC_USEFUL_JS.js
@@ -47,3 +47,16 @@ document.getElementById("demo").innerHTML = html;
 // </script>
 // </body>
 // </html>
+
+
+
+// String Search Methods
+
+// String indexOf()
+// String lastIndexOf()
+// String search()
+// String match()
+// String matchAll()
+// String includes()
+// String startsWith()
+// String endsWith()
