7307888ed18a01729bace3f71a20725f412a75a1
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-08-24 02:08:51 +0500

    Update BASIC_USEFUL_JS.js

--- BASIC_USEFUL_JS.js
+++ BASIC_USEFUL_JS.js
@@ -60,3 +60,26 @@ document.getElementById("demo").innerHTML = html;
 // String includes()
 // String startsWith()
 // String endsWith()
+
+
+
+Basic Methods
+Basic number methods can be used on any number:
+
+toString()
+toExponential()
+toFixed()
+toPrecision()
+valueOf()
+
+
+
+Static Methods
+Static methods can only be used on Number:
+
+Number.isFinite()
+Number.isInteger()
+Number.isNan()
+Number.isSafeInteger()
+Number.parseInt()
+Number.parseFloat()
