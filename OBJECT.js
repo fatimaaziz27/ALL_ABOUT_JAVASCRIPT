af2d4355edcde64bc318f58b09649bb261d57cd3
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-08-27 04:28:45 +0500

    Update OBJECT.js

--- OBJECT.js
+++ OBJECT.js
@@ -1,28 +1,17 @@
-What are Object Methods?
-Methods are actions that can be performed on objects.
-
-Methods are functions stored as property values.
-
-Example
+// Object Methods
+// Example
 const person = {
   firstName: "John",
   lastName: "Doe",
   age: 50,
+  
   fullName: function() {
     return this.firstName + " " + this.lastName;
   }
 };
-Property	Value
-firstName	John
-lastName	Doe
-age	50
-fullName	function() {
-  return this.firstName + " " + this.lastName;
-}
-The this Keyword
-In an object method, this refers to the object.
 
-Example 1
+// The this Keyword
+// Example 1
 const person = {
   firstName: "John",
   lastName: "Doe",
@@ -31,13 +20,9 @@ const person = {
     return this.id;
   }
 };
-
 let number = person.getId();
-In the example above, this refers to the person object.
 
-this.id means the id property of the person object.
-
-Example 2
+// Example 2
 const person = {
   firstName: "John",
   lastName: "Doe",
@@ -46,82 +31,29 @@ const person = {
     return this.firstName + " " + this.lastName;
   }
 };
-In the example above, this refers to the person object.
 
-this.firstName means the firstName property of the person object.
-
-this.lastName means the lastName property of the person object.
-
-Accessing Object Methods
-To call an object method, add parentheses ():
-
-Without parentheses you get the function itself.
-
-Syntax
-objectName.methodName()
-If you call a method with parentheses, it will execute as a function:
-
-Example
+// Accessing Object Methods
+// Syntax
+// objectName.methodName()
+// Example
 name = person.fullName();
-If you call a method without parentheses, it will return the function definition:
 
-Example
-name = person.fullName;
-REMOVE ADS
-
-Adding a Method to an Object
-You can add a method to an object by assigning a function to a property:
-
-Example
+// Adding a method to an object by assigning a function to a property:
+// Example
 // Assign person.name to a function
 person.name = function () {
   return this.firstName + " " + this.lastName;
 };
-In the example above, person.name is a property with a function assigned to it.
 
-Adding a JavaScript Method
-This example uses the JavaScript toUpperCase() method to convert a text to uppercase:
-
-Example
+// Adding a JavaScript Method
+// toUpperCase() method 
+// Example
 person.name = function () {
   return (this.firstName + " " + this.lastName).toUpperCase();
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-JavaScript this Keyword
-this in Objects
-The this keyword refers to an object.
-
-In JavaScript, this is used to access the object that is calling a method.
-
-this in an Object Method
-When used inside an object method, this refers to the object.
-
-Example
+// this Keyword
+// Example
 const person = {
   firstName: "John",
   lastName: "Doe",
@@ -130,14 +62,9 @@ const person = {
     return this.firstName + " " + this.lastName;
   }
 };
-In the example above:
 
-this.firstName refers to the firstName property of the person object
-this.lastName refers to the lastName property of the person object
-Why Use this?
-The this keyword makes it possible to use the same method with different objects.
-
-Example
+// The this keyword makes it possible to use the same method with different objects.
+// Example
 const person1 = {
   name: "John",
   hello: function() {
@@ -154,86 +81,19 @@ const person2 = {
 
 document.getElementById("demo").innerHTML = person1.hello();
 
-REMOVE ADS
-
-this Alone
-When used alone, this refers to the global object.
-
-In a browser, the global object is the window object.
-
-Example
-let x = this;
-document.getElementById("demo").innerHTML = x;
-In strict mode, this is undefined when used alone.
-
-this in a Function
-In a regular function, this also refers to the global object.
-
-Example
-function myFunction() {
-  return this;
-}
-
-document.getElementById("demo").innerHTML = myFunction();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-JavaScript Display Objects
-How to Display JavaScript Objects?
-Displaying a JavaScript object will output [object Object].
-
-Example
+// JavaScript Display Objects
+// Example
 // Create an Object
 const person = {
   name: "John",
   age: 30,
   city: "New York"
 };
-
-let text = person;
-Why do I See [object Object]?
-[Object Object] appears when you try to put an object (a data structure with properties) into a context where a string is expected.
-
-[Object Object] how JavaScript deals with this situation.
-
-Some solutions to display JavaScript objects are:
-
-Displaying the Object Properties by name
-Displaying the Object Properties in a Loop
-Displaying the Object using Object.values()
-Displaying the Object using JSON.stringify()
-Displaying Object Properties
-The properties of an object can be added in a string:
-
-Example
-// Create an Object
-const person = {
-  name: "John",
-  age: 30,
-  city: "New York"
-};
-
 // Add Properties
 let text = person.name + "," + person.age + "," + person.city;
-Using a For .. In Loop
-The properties of an object can be collected in a loop:
 
-Example
+// Using a For .. In Loop
+// Example
 // Create an Object
 const person = {
   name: "John",
